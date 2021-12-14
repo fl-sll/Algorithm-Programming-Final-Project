01df95d300147691cfba2e1c9427ee0e209e3092
fl-sll <edwardalvin@gmail.com>
2021-12-14 23:10:22 +0700

    imported modules and make board random

--- SudokuGrid.py
+++ SudokuGrid.py
@@ -1,17 +1,11 @@
 import pygame
 import time
+import numpy as np
+from dokusan import generators
 from solver import solve,valid
 pygame.font.init()
 
 class Grid:
-    board = [
-        [9, 2, 0, 0, 0, 0, 5, 8, 4],
-        [0, 0, 0, 5, 0, 0, 0, 0, 3],
-        [0, 8, 3, 0, 9, 2, 0, 0, 0],
-        [2, 6, 0, 8, 5, 4, 0, 0, 1],
-        [0, 0, 5, 3, 6, 1, 0, 9, 0],
-        [1, 0, 0, 0, 0, 9, 0, 0, 0],
-        [8, 5, 0, 2, 0, 3, 0, 1, 0],
-        [4, 1, 2, 9, 8, 0, 0, 3, 0],
-        [3, 9, 0, 0, 0, 6, 8, 0, 0]
-    ]
+    grid = np.array(list(str(generators.random_sudoku(avg_rank = 150))))
+    bo = grid.reshape(9,9)
+    board = bo.astype(int)
