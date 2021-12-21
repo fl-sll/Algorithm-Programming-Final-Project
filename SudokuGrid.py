98562495380bb84598898797dea142ef16a2ea68
fl-sll <edwardalvin@gmail.com>
2021-12-21 16:36:10 +0700

    created constructor, update_model and new object

--- SudokuGrid.py
+++ SudokuGrid.py
@@ -6,6 +6,25 @@ from solver import solve,valid
 pygame.font.init()
 
 class Grid:
+    # Create board for the Sudoku
     grid = np.array(list(str(generators.random_sudoku(avg_rank = 150))))
     bo = grid.reshape(9,9)
     board = bo.astype(int)
+    
+    # Create the constructor for the object
+    def __init__(self, rows, cols, width, height):
+        self.rows = rows
+        self.cols = cols
+        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(9)] for i in range(9)]
+        self.width = width
+        self.height = height
+        self.model = None
+        self.selected = None
+
+    # function to update the model
+    def update_model(self):
+        self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
+
+class Cube:
+    rows = 9
+    cols = 9
